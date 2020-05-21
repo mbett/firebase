4fe22563a59624e4c0df11f42b2fb977d80eb2ca
mbett <mbett@cs.cmu.edu>
2020-05-21 17:41:33 -0400

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,27 @@
+
+function initializeFirebase() {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC9eDzCtHrP4ZTwoB7puT0R2YA39lD-1mg",
+    authDomain: "introduction-2-web-programming.firebaseapp.com",
+    databaseURL: "https://introduction-2-web-programming.firebaseio.com",
+    projectId: "introduction-2-web-programming",
+    storageBucket: "introduction-2-web-programming.appspot.com",
+    messagingSenderId: "1096908921815",
+    appId: "1:1096908921815:web:06b956148d358dbd36c992",
+    measurementId: "G-ZFPVE55QKH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+initializeFirebase();
